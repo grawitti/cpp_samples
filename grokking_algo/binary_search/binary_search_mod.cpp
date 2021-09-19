7edd1a3b37a7c9d36b6115e88276cb2bf38a18ed
Kurt Kobain <acinonyx85@gmail.com>
2021-09-19 13:31:52 +0500

    add grokking_algo

---
+++ grokking_algo/binary_search/binary_search_mod.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binary_search(vector<int>& list, int item)
+{
+    int low = 0;
+    int high = list.size() - 1;
+
+    int i = 0;
+    while (low != high) {
+	cout << ++i << endl;
+	int mid = (low + high);
+	int guess = list.at(mid);
+
+	if (guess == item) {
+	    return mid;
+	}
+
+	if (guess > item) {
+	    high = mid / 2;
+	}
+	else {
+	    low = mid / 2;
+	}
+    }
+    
+    return -1;
+}
+
+int main()
+{
+    vector<int> my_list = {1, 3, 5, 7, 9};
+
+    cout << binary_search(my_list, 3) << endl; // => 1
+    cout << endl;
+    cout << binary_search(my_list, -1) << endl; // => -1
+
+    return 0;
+}
