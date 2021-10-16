3f84aff35ba820925e27cfdda72747007bc9064b
Kurt Kobain <acinonyx85@gmail.com>
2021-10-16 13:58:56 +0500

    fix binary_search_mod algorithm

--- grokking_algo/binary_search/binary_search_mod.cpp
+++ grokking_algo/binary_search/binary_search_mod.cpp
@@ -9,9 +9,8 @@ int binary_search(vector<int>& list, int item)
     int high = list.size() - 1;
 
     int i = 0;
-    while (low != high) {
-	cout << ++i << endl;
-	int mid = (low + high);
+    while (low <= high) {
+	int mid = (low + high) / 2;
 	int guess = list.at(mid);
 
 	if (guess == item) {
@@ -19,10 +18,10 @@ int binary_search(vector<int>& list, int item)
 	}
 
 	if (guess > item) {
-	    high = mid / 2;
+	    high = mid - 1;
 	}
 	else {
-	    low = mid / 2;
+	    low = mid + 1;
 	}
     }
     
@@ -34,8 +33,8 @@ int main()
     vector<int> my_list = {1, 3, 5, 7, 9};
 
     cout << binary_search(my_list, 3) << endl; // => 1
-    cout << endl;
     cout << binary_search(my_list, -1) << endl; // => -1
+    cout << binary_search(my_list, 11) << endl; // => -1
 
     return 0;
 }
